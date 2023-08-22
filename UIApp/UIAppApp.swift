be76278353f5f95a9aae903b3a9328957595b273
raxeno0 <aksenov2389_z@mail.ru>
2023-08-22 20:07:54 +0300

    Initial Commit

---
+++ UIApp/UIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIAppApp.swift
+//  UIApp
+//
+//  Created by Roman Axenov on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
